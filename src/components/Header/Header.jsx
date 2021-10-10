0331d4687bf8bcb511c1c85f55f125a394a52b88
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2021-10-11 01:48:32 +0600

    right sidebar is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
-import { AiOutlineSearch } from "react-icons/ai";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./Header.css";
 const Header = () => {
   return (
@@ -11,11 +10,7 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <Nav.Link href="#action1" className="fs-5 fw-bold">
               Movie
             </Nav.Link>
@@ -23,18 +18,6 @@ const Header = () => {
               Tv Show
             </Nav.Link>
           </Nav>
-          <Form className="d-flex me-3 position-relative">
-            <FormControl
-              pill
-              type="search"
-              placeholder="Search"
-              className="me-2 border-0 search ms-2 p-2"
-              aria-label="Search"
-            />
-            <span className="position-absolute ms-3 mt-2">
-              <AiOutlineSearch size="1.4rem" />
-            </span>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
