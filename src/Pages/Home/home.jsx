eb918d0e189af5750c73d50be17a03e8c89a7953
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2021-10-13 18:18:33 +0600

    updated

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -7,22 +7,20 @@ import RightSidebar from "../../components/RightSideBar/RightSidebar";
 import "./Home.css";
 const home = () => {
   return (
-    <>
-      <div>
-        <Header />
-        <Row className="home__container justify-content-between">
-          <Col md={2} className="border-end pe-0">
-            <LeftSideBar />
-          </Col>
-          <Col md={7}>
-            <Main />
-          </Col>
-          <Col md={3}>
-            <RightSidebar />
-          </Col>
-        </Row>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Row className="home__container justify-content-between">
+        <Col md={2} className="border-end pe-0">
+          <LeftSideBar />
+        </Col>
+        <Col md={7}>
+          <Main />
+        </Col>
+        <Col md={3}>
+          <RightSidebar />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
