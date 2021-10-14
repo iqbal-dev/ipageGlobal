1e69bcb2ed5593af92e7f6c0e583ce2b39bf701e
iqbal-dev <pkmdiqbal@gmail.com>
2021-10-14 14:26:49 +0600

    done

--- src/components/RightSideBar/RightSidebar.jsx
+++ src/components/RightSideBar/RightSidebar.jsx
@@ -17,38 +17,42 @@ const RightSidebar = () => {
     dispatch(fetchPopularTvShow());
   }, [dispatch]);
   return (
-    <div className="px-4 border-start right__container pb-5">
-      <Form className="d-flex position-relative">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2 border-0 search ms-2 px-2 py-2"
-          aria-label="Search"
-        />
-        <span className="position-absolute ms-3 mt-2">
-          <AiOutlineSearch size="1.4rem" />
-        </span>
-      </Form>
-      <div className="mb-3 mt-4 mx-1 text-uppercase fw-bold text-black-50">
-        Popular Movies
-      </div>
-      {results?.popularMovie?.results
-        ?.filter((movie, index) => index < 2)
-        ?.map((data) => (
-          <MovieWithShortDetails movieData={data} key={data.id} />
-        ))}
-      <Button className="me-2 border-0">See More</Button>
-      <div className="mb-3 mt-4 mx-1 text-uppercase fw-bold text-black-50">
-        WatchLists
-      </div>
-      {results?.watchLists?.results
-        ?.filter((movie, index) => index < 4)
-        ?.map((data) => (
-          <MovieWithShortDetails movieData={data} key={data.id} />
-        ))}
+    <>
+      {!results?.isLoading && (
+        <div className="px-4 border-start right__container pb-5">
+          <Form className="d-flex position-relative">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2 border-0 search ms-2 px-2 py-2"
+              aria-label="Search"
+            />
+            <span className="position-absolute ms-3 mt-2">
+              <AiOutlineSearch size="1.4rem" />
+            </span>
+          </Form>
+          <div className="mb-3 mt-4 mx-1 text-uppercase fw-bold text-black-50">
+            Popular Movies
+          </div>
+          {results?.popularMovie?.results
+            ?.filter((movie, index) => index < 2)
+            ?.map((data) => (
+              <MovieWithShortDetails movieData={data} key={data.id} />
+            ))}
+          <Button className="me-2 border-0">See More</Button>
+          <div className="mb-3 mt-4 mx-1 text-uppercase fw-bold text-black-50">
+            WatchLists
+          </div>
+          {results?.watchLists?.results
+            ?.filter((movie, index) => index < 4)
+            ?.map((data) => (
+              <MovieWithShortDetails movieData={data} key={data.id} />
+            ))}
 
-      <Button className="me-2 border-0">See More</Button>
-    </div>
+          <Button className="me-2 border-0">See More</Button>
+        </div>
+      )}
+    </>
   );
 };
 
